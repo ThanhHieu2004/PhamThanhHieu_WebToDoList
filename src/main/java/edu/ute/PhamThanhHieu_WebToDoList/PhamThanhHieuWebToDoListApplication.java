613ab200141439d8e4cfb67f103e7de8cab72f04
Thanh Hieu <thanhhieu8304@gmail.com>
2025-05-06 17:26:14 +0700

    Initial commit

---
+++ src/main/java/edu/ute/PhamThanhHieu_WebToDoList/PhamThanhHieuWebToDoListApplication.java
@@ -0,0 +1,13 @@
+package edu.ute.PhamThanhHieu_WebToDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamThanhHieuWebToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamThanhHieuWebToDoListApplication.class, args);
+	}
+
+}
