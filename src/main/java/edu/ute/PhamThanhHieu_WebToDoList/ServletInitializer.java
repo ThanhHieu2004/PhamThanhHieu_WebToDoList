5865f3ee2d34dd50e6540210064b37f2d6935c6d
Thanh Hieu <thanhhieu8304@gmail.com>
2025-05-06 17:26:14 +0700

    Initial commit

---
+++ src/main/java/edu/ute/PhamThanhHieu_WebToDoList/ServletInitializer.java
@@ -0,0 +1,13 @@
+package edu.ute.PhamThanhHieu_WebToDoList;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PhamThanhHieuWebToDoListApplication.class);
+	}
+
+}
---
+++ src/test/java/edu/ute/PhamThanhHieu_WebToDoList/PhamThanhHieuWebToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ute.PhamThanhHieu_WebToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhamThanhHieuWebToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
